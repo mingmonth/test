080c2f1f2742d72c4d216ee3b6b7be9cda7ffa30
yoonsoo <yoonsoo@hknetworks.kr>
2018-08-01 18:59:16 +0900

    Main2 클래스 두 줄만 남기기

--- src/main/java/io/mingmonth/inflearn/intellij/Main2.java
+++ src/main/java/io/mingmonth/inflearn/intellij/Main2.java
@@ -4,8 +4,6 @@ public class Main2 {
     public static void main(String[] args) {
         System.out.println("bbb");
         System.out.println("bbb");
-        System.out.println("bbb");
-        System.out.println("bbb");
-        System.out.println("bbb");
+
     }
 }
